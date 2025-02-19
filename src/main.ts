924af00da12f5f6cefb2038d5dfd9532d0b50da8
Igor Rodrigues Lage <65911049+igor-rl@users.noreply.github.com>
2025-02-18 21:23:58 -0300

    chore

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api'); // Define o prefixo "/api" para todas as rotas
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
